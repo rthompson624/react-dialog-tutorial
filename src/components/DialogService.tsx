b3939ca755d245145213714733e419ddf04a1011
Richard Thompson <rthompson624@gmail.com>
2022-03-19 13:14:10 -0400

    Set min/max width

--- src/components/DialogService.tsx
+++ src/components/DialogService.tsx
@@ -13,9 +13,9 @@ export default function DialogService (props: IDialogServiceProps) {
     : "fixed top-0 left-0 w-full h-full bg-gray-300 bg-opacity-70 hidden"
   return (
     <div className={ containerClassName }>
-      <div className="bg-white w-1/2 h-1/2 p-2 shadow-2xl rounded-lg">
+      <div className="bg-white min-w-[50%] md:min-w-[40%] lg:min-w-[30%] max-w-[85%] p-2 shadow-2xl rounded-lg">
         <div className="flex justify-between items-center">
-          <div>{ props.title }</div>
+          <div className="font-bold">{ props.title }</div>
           <div>
             <IoIosCloseCircleOutline onClick={ () => props.onClose() } className="text-xl cursor-pointer" />
           </div>
